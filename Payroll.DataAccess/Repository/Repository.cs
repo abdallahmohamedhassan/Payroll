1922b7839a202c47f7426428ef059ad1c18fac4c
abdallahmohamedhassan <abdallah.hassan.eg@gmail.com>
2025-05-23 21:33:41 +0300

    application dbcontext generic repository unit of work

---
+++ Payroll.DataAccess/Repository/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payroll.DataAccess.Data;
+using Payroll.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {private readonly ApplicationDbContext _context;
+        internal DbSet<T> DbSet;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            this.DbSet = _context.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> query = DbSet;
+            query=query.Where(filter);
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var property in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                { query = query.Include(property); }
+            }
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(string? includeProperties =null)
+        {
+            IQueryable<T> query = DbSet;
+            if(!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach(var property in includeProperties.Split(new char[] {','},StringSplitOptions.RemoveEmptyEntries))
+                { query = query.Include(property); }
+            }
+            return query.ToList();
+
+        }
+
+        public void Remove(T entity)
+        {
+            DbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            DbSet.RemoveRange(entities);
+        }
+        public void Update(T entity)
+        {
+            DbSet.Update(entity);
+        }
+
+    }
+}
---
+++ Payroll.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Payroll.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        ApplicationDbContext _db;
+       public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            
+         }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
